4a3e1916afd60a68738eb6978aa8ccb039f2453f
briankundinger <briankundinger@gmail.com>
2024-04-28 19:15:55 -0400

    update vabldev

---
+++ scripts/update_vabldev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --mail-type=end
+#SBATCH --mail-user=bak47@duke.edu
+#SBATCH --output=logs/%x.out
+#SBATCH --error=logs/%x.err
+#SBATCH --job-name=update_vabldev
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=16G
+
+Rscript code/update_vabldev.R
