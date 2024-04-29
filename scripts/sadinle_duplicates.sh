770b04a370855804324fb8bfcdd81cdf67d84e66
briankundinger <briankundinger@gmail.com>
2024-04-29 12:26:26 -0400

    sadinle sims

--- scripts/sadinle_duplicates.sh
+++ scripts/sadinle_duplicates.sh
@@ -7,6 +7,6 @@
 #SBATCH --error=logs/%x_%a.err
 #SBATCH --job-name=sadinle_duplicates
 #SBATCH --time=24:00:00
-#SBATCH --mem-per-cpu=16G
+#SBATCH --mem-per-cpu=64G
 
 Rscript code/sadinle_duplicates.R
