0785811f770603c6b3bc05ad3c5e962bb37017e4
briankundinger <briankundinger@gmail.com>
2024-04-29 12:29:23 -0400

    edits

--- scripts/ncvr_combine.sh
+++ scripts/ncvr_combine.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH -p compsci
-#SBATCH --array=1
 #SBATCH --mail-type=end
 #SBATCH --mail-user=bak47@duke.edu
-#SBATCH --output=logs/%x_%a.out
-#SBATCH --error=logs/%x_%a.err
+#SBATCH --output=logs/%x.out
+#SBATCH --error=logs/%x.err
 #SBATCH --job-name=ncvr_combine
 #SBATCH --time=24:00:00
-#SBATCH --mem-per-cpu=512G
+#SBATCH --mem-per-cpu=256G
 #
 # modules
 #
