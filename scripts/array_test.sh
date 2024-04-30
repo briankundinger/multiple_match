c329f492c247081c6aacca96df13faa01a51614b
briankundinger <briankundinger@gmail.com>
2024-04-30 19:56:20 -0400

    array test

---
+++ scripts/array_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p compsci
+#SBATCH --array=1-999
+#SBATCH --mail-type=end
+#SBATCH --mail-user=bak47@duke.edu
+#SBATCH --output=logs/%x_%a.out
+#SBATCH --error=logs/%x_%a.err
+#SBATCH --job-name=array_test
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=1M
+
+
