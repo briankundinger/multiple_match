5e812b68cb11f6e4bed6f536cc8f020e0aedc180
briankundinger <briankundinger@gmail.com>
2024-04-30 19:11:32 -0400

    ncvr

--- scripts/ncvr_hash.sh
+++ scripts/ncvr_hash.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #SBATCH -p compsci
-#SBATCH --array=1-100%100
+#SBATCH --array=800-900%100
 #SBATCH --mail-type=end
 #SBATCH --mail-user=bak47@duke.edu
 #SBATCH --output=logs/%x_%a.out
 #SBATCH --error=logs/%x_%a.err
 #SBATCH --job-name=ncvr_hash
 #SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=64G
 
 Rscript code/ncvr_hash.R
