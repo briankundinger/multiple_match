4e886f0d71a9c1f8cdec0fbe8231649d2dad6214
briankundinger <briankundinger@gmail.com>
2024-05-03 11:55:28 -0400

    overlap 100

--- code/sadinle_duplicates.R
+++ code/sadinle_duplicates.R
@@ -25,7 +25,7 @@ tmax= 200
 threshold = 1e-6
 resolve = T
 
-overlap <- 250
+overlap <- 100
 
 records <- read_csv(files[i], col_types = cols())
 records$file <- rep(2:1, length.out = dim(records)[1])
