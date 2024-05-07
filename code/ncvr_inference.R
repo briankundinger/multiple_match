5fc7830e2a416d2130af836930bd493c3b2c17bd
briankundinger <briankundinger@gmail.com>
2024-05-07 16:36:46 -0400

    inference

--- code/ncvr_inference.R
+++ code/ncvr_inference.R
@@ -2,8 +2,8 @@ library(vabldev)
 
 ncvr_a <- readRDS("data/ncvr_a")
 ncvr_b <- readRDS("data/ncvr_b")
-S <- 10
-burn <- S * .1
+S <- 100
+burn <- ceiling(S * .1)
 tmax = 1000
 
 df1 <- ncvr_a %>%
@@ -36,7 +36,8 @@ ptm <- proc.time()
 chain <- fabl(hash, S = S, burn = burn)
 seconds <- proc.time() - ptm
 results <- estimate_links(chain, hash)
-eval <- evaluate_links(results$Z_hat, Z_true_pairs, n1, "pairs")
+Z_hat <- make_Zhat_pairs(results$Z_hat)
+eval <- evaluate_links(Z_hat, Z_true_pairs, n1, "pairs")
 df <- data.frame(n1 = n1,
                  n2 = n2,
                  recall = eval[1],
@@ -48,61 +49,62 @@ df <- data.frame(n1 = n1,
                       data = "ncvr")
 saveRDS(df, "out/ncvr_results/fabl")
 
-ptm <- proc.time()
-chain <- fabl_mm(hash, S = S, burn = burn)
-seconds <- proc.time() - ptm
-results <- estimate_links(chain, hash)
-eval <- evaluate_links(results$Z_hat, Z_true_pairs, n1, "pairs")
-df <- data.frame(n1 = n1,
-                 n2 = n2,
-                 recall = eval[1],
-                 precision = eval[2],
-                 f_measure = eval[3],
-                 iterations = S,
-                 time = seconds[3],
-                 method = "fabl",
-                 data = "ncvr")
-saveRDS(df, "out/ncvr_results/fabl_mm")
-
 # ptm <- proc.time()
-# out <- variational_fastlink(hash, tmax = tmax)
+# chain <- fabl_mm(hash, S = S, burn = burn)
 # seconds <- proc.time() - ptm
-#
-# files <- list.files(path = "out/ncvr/hash/", full.names = T)
-# file_1_labs <- 1:n1
-#
-# batch_size <- 100
-#
-# normal_batches <- n2 %/% batch_size
-# last_batch <- n2 %% batch_size
-# fs_matches <- list()
-#
-# for(i in 1:length(files)){
-#   batch <- readRDS(files[i])
-#
-#
-#   batch_id <-c(rep(1:normal_batches, each = batch_size), rep(normal_batches + 1, last_batch))
-#   batch_labs <- df2[batch_id == i, ]$rn
-#   ids <- expand.grid(file_1_labs, batch_labs)
-#   fs_matches[[i]] <- data.frame(id_1 = ids[, 1],
-#              id_2 = ids[, 2],
-#              prob = out$fs_probs[batch$hash_id]) %>%
-#     filter(prob > .5)
-# }
-#
-# fs_df <- do.call(rbind, fs_matches)
-#
-# eval <- evaluate_links(fs_matches[, 1:2], Z_true_pairs, n1, "pairs")
+# results <- estimate_links(chain, hash)
+# Z_hat <- make_Zhat_pairs(results$Z_hat)
+# eval <- evaluate_links(Z_hat, Z_true_pairs, n1, "pairs")
 # df <- data.frame(n1 = n1,
 #                  n2 = n2,
 #                  recall = eval[1],
 #                  precision = eval[2],
 #                  f_measure = eval[3],
-#                  iterations = tmax,
+#                  iterations = S,
 #                  time = seconds[3],
-#                  method = "fastlink",
+#                  method = "fabl",
 #                  data = "ncvr")
-# saveRDS(df, "out/ncvr_results/fastlink")
+# saveRDS(df, "out/ncvr_results/fabl_mm")
+
+ptm <- proc.time()
+out <- variational_fastlink(hash, tmax = tmax)
+seconds <- proc.time() - ptm
+
+files <- list.files(path = "out/ncvr/hash/", full.names = T)
+file_1_labs <- 1:n1
+
+batch_size <- 100
+
+normal_batches <- n2 %/% batch_size
+last_batch <- n2 %% batch_size
+fs_matches <- list()
+
+for(i in 1:length(files)){
+  batch <- readRDS(files[i])
+
+
+  batch_id <-c(rep(1:normal_batches, each = batch_size), rep(normal_batches + 1, last_batch))
+  batch_labs <- df2[batch_id == i, ]$rn
+  ids <- expand.grid(file_1_labs, batch_labs)
+  fs_matches[[i]] <- data.frame(id_1 = ids[, 1],
+             id_2 = ids[, 2],
+             prob = out$fs_probs[batch$hash_id]) %>%
+    filter(prob > .5)
+}
+
+fs_df <- do.call(rbind, fs_matches)
+
+eval <- evaluate_links(fs_df[, 1:2], Z_true_pairs, n1, "pairs")
+df <- data.frame(n1 = n1,
+                 n2 = n2,
+                 recall = eval[1],
+                 precision = eval[2],
+                 f_measure = eval[3],
+                 iterations = tmax,
+                 time = seconds[3],
+                 method = "fastlink",
+                 data = "ncvr")
+saveRDS(df, "out/ncvr_results/fastlink")
 
 # trace_df <- data.frame(data = "ncvr",
 #                        trace = chain$overlap) %>%
