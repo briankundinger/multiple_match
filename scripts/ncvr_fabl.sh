6dd9b33353bd8057fb40cbddacbde02e3b87250e
briankundinger <briankundinger@gmail.com>
2024-05-09 16:57:40 -0400

    lets goooo

---
+++ scripts/ncvr_fabl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -p compsci
+#SBATCH --mail-type=end
+#SBATCH --mail-user=bak47@duke.edu
+#SBATCH --output=logs/%x.out
+#SBATCH --error=logs/%x.err
+#SBATCH --job-name=ncvr_fabl
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=200G
+#
+# modules
+#
+
+Rscript code/ncvr_fabl.R
--- scripts/ncvr_fabl_mm_2.sh
+++ scripts/ncvr_fabl_mm_2.sh
@@ -4,11 +4,11 @@
 #SBATCH --mail-user=bak47@duke.edu
 #SBATCH --output=logs/%x.out
 #SBATCH --error=logs/%x.err
-#SBATCH --job-name=ncvr_inference
+#SBATCH --job-name=ncvr_fabl_mm_2
 #SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu=200G
 #
 # modules
 #
 
-Rscript code/ncvr_inference.R
+Rscript code/ncvr_fabl_mm_2.R
