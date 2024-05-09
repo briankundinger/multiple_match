b73dfe28767d3802fcdd144a651444e3e837fc1f
briankundinger <briankundinger@gmail.com>
2024-05-09 18:37:50 -0400

    fabl

--- code/ncvr_fabl.R
+++ code/ncvr_fabl.R
@@ -19,7 +19,7 @@ df2 <- ncvr_b %>%
 
 n1 <- nrow(df1)
 n2 <- nrow(df2)
-
+print(1)
 joined <- right_join(df1, df2, by = "voter_id", copy = T, keep = T,
                      relationship = "many-to-many") %>%
   arrange(voter_id.y)
@@ -31,12 +31,14 @@ Z_true_pairs <- joined %>%
 # Z_true <- joined$rn
 
 hash <- readRDS("out/ncvr/combine/hash")
-
+print(2)
 ptm <- proc.time()
 chain <- fabl(hash, S = S, burn = burn)
+print(3)
 seconds <- proc.time() - ptm
-results <- estimate_links(chain, hash)
+print(4)
 Z_hat <- make_Zhat_pairs(results$Z_hat)
+print(5)
 saveRDS(Z_hat, "out/ncvr_results/Z_hat/fabl")
 eval <- evaluate_links(Z_hat, Z_true_pairs, n1, "pairs")
 df <- data.frame(n1 = n1,
@@ -51,23 +53,6 @@ df <- data.frame(n1 = n1,
 saveRDS(df, "out/ncvr_results/eval/fabl")
 
 ptm <- proc.time()
-chain <- fabl_mm(hash, S = S, burn = burn)
-seconds <- proc.time() - ptm
-results <- estimate_links(chain, hash)
-Z_hat <- make_Zhat_pairs(results$Z_hat)
-eval <- evaluate_links(Z_hat, Z_true_pairs, n1, "pairs")
-df <- data.frame(n1 = n1,
-                 n2 = n2,
-                 recall = eval[1],
-                 precision = eval[2],
-                 f_measure = eval[3],
-                 iterations = S,
-                 time = seconds[3],
-                 method = "fabl",
-                 data = "ncvr")
-saveRDS(df, "out/ncvr_results/fabl_mm")
-
-
 
 # trace_df <- data.frame(data = "ncvr",
 #                        trace = chain$overlap) %>%
