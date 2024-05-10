b883b45b8f4b2d702bb0f7a4f4a2aebba1f6c9d7
briankundinger <briankundinger@gmail.com>
2024-05-10 19:11:07 -0400

    40

--- scripts/core_test.sh
+++ scripts/core_test.sh
@@ -2,11 +2,11 @@
 #SBATCH -p compsci
 #SBATCH --mail-type=end
 #SBATCH --mail-user=bak47@duke.edu
-#SBATCH --output=logs/%x_%a.out
-#SBATCH --error=logs/%x_%a.err
+#SBATCH --output=logs/%x.out
+#SBATCH --error=logs/%x.err
 #SBATCH --job-name=core_test
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=100
+#SBATCH --cpus-per-task=40
 
 Rscript code/core_test.R
