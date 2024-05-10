8df2dc8ed80bd9cccc67eb3c1805aeddb6a006b1
briankundinger <briankundinger@gmail.com>
2024-05-10 19:03:24 -0400

    try

--- scripts/ncvr_fastlink.sh
+++ scripts/ncvr_fastlink.sh
@@ -6,9 +6,9 @@
 #SBATCH --error=logs/%x.err
 #SBATCH --job-name=ncvr_fastlink
 #SBATCH --time=24:00:00
-#SBATCH --mem=256G
+#SBATCH --mem=201G
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=40
+#SBATCH --cpus-per-task=20
 #
 # modules
 #
