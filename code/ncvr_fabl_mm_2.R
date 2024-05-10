2c5d573b46a6108373a59282d2fdd0f6584f77c0
briankundinger <briankundinger@gmail.com>
2024-05-10 16:53:32 -0400

    k=1

--- code/ncvr_fabl_mm_2.R
+++ code/ncvr_fabl_mm_2.R
@@ -34,7 +34,7 @@ hash <- readRDS("out/ncvr/combine/hash")
 
 ptm <- proc.time()
 print(1)
-chain <- fabl_mm(hash, S = S, burn = burn, max_K = 2)
+chain <- fabl_mm(hash, S = S, burn = burn, max_K = 1)
 saveRDS(chain, "out/ncvr_results/chain/fabl_mm_2")
 seconds <- proc.time() - ptm
 print(2)
