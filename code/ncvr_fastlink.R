9b56cb9d2af9fc59d40126bb75e27726b1bdee72
briankundinger <briankundinger@gmail.com>
2024-05-11 00:55:17 -0400

    lets goooo

--- code/ncvr_fastlink.R
+++ code/ncvr_fastlink.R
@@ -36,14 +36,16 @@ ptm <- proc.time()
 fl_out <- fastLink::fastLink(ncvr_a, ncvr_b, varnames = names(ncvr_a)[c(4, 5, 6, 7, 9, 10)],
                    stringdist.match = names(ncvr_a)[c(4, 6)],
                    partial.match = names(ncvr_a)[c(4, 6)],
+                   stringdist.method = "lv",
                    cut.a = 1, cut.p = .75, dedupe.matches = F, threshold.match = .5,
-                   n.cores = cores)
+                   n.cores = cores, verbose = T, return.all = F, tol.em = 1e-07, )
 seconds <- proc.time() - ptm
 
 Z_hat <- data.frame(id_1 = fl_out$matches$inds.a,
                     id_2 = fl_out$matches$inds.b)
 
 saveRDS(Z_hat, "out/ncvr_results/Z_hat/fastlink")
+saveRDS(fl_out$posterior, "out/ncvr_results/prob/fastlink")
 
 eval <- evaluate_links(Z_hat, Z_true_pairs, n1, "pairs")
 df <- data.frame(n1 = n1,
