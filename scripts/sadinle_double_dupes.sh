2d9d76def85f252779b9b1f0c1f4a08bfcbb497b
briankundinger <briankundinger@gmail.com>
2024-05-19 19:03:41 -0400

    double dupes

--- scripts/sadinle_double_dupes.sh
+++ scripts/sadinle_double_dupes.sh
@@ -7,7 +7,6 @@
 #SBATCH --error=logs/%x_%a.err
 #SBATCH --job-name=sadinle_double_dupes
 #SBATCH --time=24:00:00
-#SBATCH --mem-per-cpu=64G
+#SBATCH --mem-per-cpu=16G
 
-Rscript code/sadinle_double_dupes
-.R
+Rscript code/sadinle_double_dupes.R
