439ee27e8e3abcf336e5636e6e7f47009d05210b
briankundinger <briankundinger@gmail.com>
2024-05-19 19:43:07 -0400

    updated path

--- code/combine_sadinle_double_dupes.R
+++ code/combine_sadinle_double_dupes.R
@@ -4,4 +4,4 @@ files <- list.files("out/sadinle_double_dupes/", full.names = T)
 results <- lapply(files, readRDS) %>%
   do.call(rbind, .)
 
-saveRDS(results, "out/sadinle_double_dupes")
+saveRDS(results, "out/sadinle_double_dupes_all")
