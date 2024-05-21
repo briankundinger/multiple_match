3cda17baca56fbb7ac32aadbe83c5c758362f642
briankundinger <briankundinger@gmail.com>
2024-05-21 19:25:46 -0400

    typo

--- code/mms_combine.R
+++ code/mms_combine.R
@@ -9,6 +9,8 @@ files <- list.files("out/ncvr/mms/prob_batch/", full.names = T)
 prob <- lapply(files, readRDS) %>%
   do.call(c, .)
 
-saveRDS(mms, paste0("out/ncvr/mms/combine/mms")
+saveRDS(mms, paste0("out/ncvr/mms/combine/mms"))
 
-saveRDS(prob, paste0("out/ncvr/mms/combine/prob")
+saveRDS(prob, paste0("out/ncvr/mms/combine/prob"))
+
+
