97745f17934d4d19c8c0da905e8c8429f468de24
briankundinger <briankundinger@gmail.com>
2024-05-21 19:23:32 -0400

    faster

--- code/mms_combine.R
+++ code/mms_combine.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 files <- list.files("out/ncvr/mms/mms_batch/", full.names = T)
 mms <- lapply(files, readRDS) %>%
-  do.call(append, .)
+  do.call(unlist, ., recursive = F)
 
 files <- list.files("out/ncvr/mms/prob_batch/", full.names = T)
 prob <- lapply(files, readRDS) %>%
