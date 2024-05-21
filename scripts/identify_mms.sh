20e59d1dfb5048f291c155c25f24246a21788ec3
briankundinger <briankundinger@gmail.com>
2024-05-21 18:04:31 -0400

    typo

--- scripts/identify_mms.sh
+++ scripts/identify_mms.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-user=bak47@duke.edu
 #SBATCH --output=logs/%x_%a.out
 #SBATCH --error=logs/%x_%a.err
-#SBATCH --job-name=ncvr_hash
+#SBATCH --job-name=identify_mms
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
