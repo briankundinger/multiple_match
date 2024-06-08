c679312fc7138623a226ab3cc7391cba0807391d
briankundinger <briankundinger@gmail.com>
2024-06-08 15:41:27 -0400

    get_xtmp

---
+++ scripts/get_xtmp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -p compsci
+#SBATCH --array=1
+#SBATCH --mail-type=end
+#SBATCH --mail-user=bak47@duke.edu
+#SBATCH --output=logs/%x_%a.out
+#SBATCH --error=logs/%x_%a.err
+#SBATCH --job-name=get_xtmp
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=16G
+#
+# modules
+#
+
+cp ../../../../../usr/xtmp/bak47/mm/swap/eval/fabl out/xtmp/fabl_swap
+cp ../../../../../usr/xtmp/bak47/mm/b_dedupe/eval/fabl out/xtmp/fabl_dedup
