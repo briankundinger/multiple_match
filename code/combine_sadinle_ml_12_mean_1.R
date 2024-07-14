e95f7d7b08cf305a0d3a768900183422416846d0
briankundinger <briankundinger@gmail.com>
2024-07-14 14:47:27 -0400

    git ignore

---
+++ code/combine_sadinle_ml_12_mean_1.R
@@ -0,0 +1,7 @@
+library(dplyr)
+
+files <- list.files("out/sadinle_sim_ml_12_mean_1/", full.names = T)
+results <- lapply(files, readRDS) %>%
+  do.call(rbind, .)
+
+saveRDS(results, "out/sadinle_ml_all_12_mean_1")
---
+++ scripts/combine_sadinle_ml_12_mean_1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -p compsci
+#SBATCH --array=1
+#SBATCH --mail-type=end
+#SBATCH --mail-user=bak47@duke.edu
+#SBATCH --output=logs/%x_%a.out
+#SBATCH --error=logs/%x_%a.err
+#SBATCH --job-name=combine_sadinle_ml_12_mean_1
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=16G
+#
+# modules
+#
+
+Rscript code/combine_sadinle_ml_12_mean_1.R
