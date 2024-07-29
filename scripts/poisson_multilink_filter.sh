93c67eb86864ec403e2bebecccb7226b5aab943e
briankundinger <briankundinger@gmail.com>
2024-07-29 15:52:52 -0400

    fixed names

--- scripts/poisson_multilink_filter.sh
+++ scripts/poisson_multilink_filter.sh
@@ -5,8 +5,8 @@
 #SBATCH --mail-user=bak47@duke.edu
 #SBATCH --output=logs/%x_%a.out
 #SBATCH --error=logs/%x_%a.err
-#SBATCH --job-name=poisson_ml
+#SBATCH --job-name=poisson_ml_filter
 #SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu=64G
 
-Rscript code/poisson_ml.R
+Rscript code/poisson_multilink_filter.R
